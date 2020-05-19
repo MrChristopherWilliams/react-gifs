4181a98b38dc9d4980c618fd130c69789819ba08
Chris Williams <chriswilliams14.williams@gmail.com>
2020-05-19 16:19:19 +0100

    Completed first React web app

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,28 +1,49 @@
 import React, { Component } from 'react';
+import giphy from 'giphy-api';
 
 import SearchBar from './search_bar';
 import Gif from './gif';
 import GifList from './gif_list';
 
 class App extends Component {
-  render() {
-    const gifs = [
-      { id: "jUwpNzg9IcyrK" },
-      { id: "jUwpNzg9IcyrK" },
-      { id: "jUwpNzg9IcyrK" },
-      { id: "jUwpNzg9IcyrK" }
-    ];
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      gifs: [],
+      selectedGifId: "jUwpNzg9IcyrK"
+    };
+  }
+
+  search = (query) => {
+    giphy('cj5KZQeobs10JZ7H0bPY9iUw32AmW1cj').search({
+      q: query,
+      rating: 'g',
+      limit: 10
+    }, (error, result) => {
+      this.setState({
+        gifs: result.data
+      });
+    });
+  }
+
+  selectGif = (id) => {
+    this.setState({
+      selectedGifId: id
+    });
+  }
+
+  render() {
     return (
       <div>
         <div className="left-scene">
-          <SearchBar />
+          <SearchBar searchFunction={this.search} />
           <div className="selected-gif">
-            <Gif id="jUwpNzg9IcyrK"/>
+            <Gif id={this.state.selectedGifId} />
           </div>
         </div>
         <div className="right-scene">
-          <GifList gifs={gifs} />
+          <GifList gifs={this.state.gifs} selectGif={this.selectGif} />
         </div>
       </div>
     );
